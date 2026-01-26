7a27e7faabbf20f0c0182d2ede5b785eac4f0afc
takamitsu-iida <takamitsu.iida@gmail.com>
2026-01-26 12:27:49 +0900

    update

--- bin/subscribe.py
+++ bin/subscribe.py
@@ -5,6 +5,11 @@ import _confd
 import _confd.cdb as cdb
 import _confd.maapi as maapi
 
+import socket
+import _confd
+import _confd.cdb as cdb
+import _confd.maapi as maapi
+
 def run():
     cdb_sock = socket.socket()
     maapi_sock = socket.socket()
@@ -12,10 +17,16 @@ def run():
     cdb.connect(cdb_sock, cdb.DATA_SOCKET, '127.0.0.1', 4565)
     maapi.connect(maapi_sock, '127.0.0.1', 4565)
 
-    # 【修正】第5引数にログイン元IPアドレスを追加
-    # _confd.Address は、タプル (socket.AF_INET, '127.0.0.1') などを期待します
-    src_addr = _confd.Address((socket.AF_INET, '127.0.0.1'))
-    maapi.start_user_session(maapi_sock, 'admin', 'system', ['admin'], src_addr)
+    # 【修正】_confd.Address を使わず、直接タプルでアドレスを指定
+    # 形式: (socket.AF_INET, '127.0.0.1')
+    # ポートを含める必要がある場合は (socket.AF_INET, '127.0.0.1', 0)
+    src_addr = (socket.AF_INET, '127.0.0.1')
+
+    try:
+        maapi.start_user_session(maapi_sock, 'admin', 'system', ['admin'], src_addr, _confd.PROTO_TCP)
+    except TypeError:
+        # 万が一、引数の数がさらに多い・少ない場合のためのフォールバック
+        maapi.start_user_session(maapi_sock, 'admin', 'system', ['admin'], src_addr)
 
     sub_id = cdb.subscribe(cdb_sock, 1, 0, '/server-config')
     cdb.subscribe_done(cdb_sock)
@@ -24,19 +35,18 @@ def run():
 
     try:
         while True:
-            # 変更通知の待機
             sub_ids = cdb.read_subscription_socket(cdb_sock)
 
-            # トランザクション開始 (1=RUNNING, 1=READ)
+            # RUNNING=1, READ=1
             th = maapi.start_trans(maapi_sock, 1, 1)
             try:
-                # 値の取得
                 val = maapi.get_elem(maapi_sock, th, "/server-config/ip-address")
-                print(f"Config Changed! New IP: {str(val)}")
+                # val がオブジェクトなら str()、そうでなければそのまま表示
+                print(f"Config Changed! New IP: {val}")
             finally:
                 maapi.finish_trans(maapi_sock, th)
 
-            # 通知完了の同期 (1=DONE_PRIORITY)
+            # DONE_PRIORITY=1
             cdb.sync_subscription_socket(cdb_sock, 1)
 
     except KeyboardInterrupt:
