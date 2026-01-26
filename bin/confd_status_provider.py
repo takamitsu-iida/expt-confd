71c2a146702e0ef2cb2b4c9de16fca1b84574a84
takamitsu-iida <takamitsu.iida@gmail.com>
2026-01-26 17:05:20 +0900

    update

--- bin/confd_status_provider.py
+++ bin/confd_status_provider.py
@@ -28,18 +28,27 @@ def run():
     wrksock = socket.socket()
 
     import struct
+
     # IPは引き続き整数を要求されている可能性が高いです
     ip_int = struct.unpack("!I", socket.inet_aton('127.0.0.1'))[0]
+
 
     try:
         # ポート番号を "4565" (文字列) に変更
         dp.connect(ctlsock, dp.CONTROL_SOCKET, ip_int, "4565")
+        print("Connected using integer IP and string port.")
+    except TypeError:
+        # 万が一 IP も文字列に戻せと言われた場合
+        dp.connect(ctlsock, dp.CONTROL_SOCKET, '127.0.0.1', "4565")
+        print("Connected using integer IP and integer port.")
+
+    try:
+        # ポート番号を "4565" (文字列) に変更
         dp.connect(wrksock, dp.WORKER_SOCKET, ip_int, "4565")
         print("Connected using integer IP and string port.")
     except TypeError:
         # 万が一 IP も文字列に戻せと言われた場合
-        dp.connect(ctlsock, dp.CONTROL_SOCKET, ip_int, "4565")
-        dp.connect(wrksock, dp.WORKER_SOCKET, ip_int, "4565")
+        dp.connect(wrksock, dp.WORKER_SOCKET, '127.0.0.1', "4565")
         print("Connected using integer IP and integer port.")
 
     dctx = dp.init_daemon("status_provider_daemon")
