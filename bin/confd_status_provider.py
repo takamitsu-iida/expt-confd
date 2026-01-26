44b2ca707e4cfe52d8bd1d17aec2d2bfd32c9a5a
takamitsu-iida <takamitsu.iida@gmail.com>
2026-01-26 17:20:29 +0900

    update

--- bin/confd_status_provider.py
+++ bin/confd_status_provider.py
@@ -9,64 +9,51 @@ from datetime import datetime
 # 起動時刻を記録
 START_TIME = datetime.now()
 
-# --- 1. トランザクション用コールバック (必須) ---
-def trans_init(tctx):
-    # トランザクション開始時の処理（今回は特に何もしない）
-    # 返り値は OK である必要があります
-    return _confd.OK
+# --- 1. コールバック用クラスの定義 ---
+class TransCallbacks:
+    # 辞書ではなくメソッドとして定義
+    def cb_init(self, tctx):
+        return _confd.OK
 
-def trans_finish(tctx):
-    # トランザクション終了時の処理
-    return _confd.OK
+    def cb_finish(self, tctx):
+        return _confd.OK
 
-# --- 2. データ取得用コールバック ---
-def get_elem_callback(tctx, kp):
-    path = str(kp)
-    if "uptime" in path:
-        delta = datetime.now() - START_TIME
-        val = _confd.Value(str(delta).split('.')[0], _confd.C_STR)
-    elif "last-checked-at" in path:
-        now = datetime.now().strftime("%H:%M:%S")
-        val = _confd.Value(now, _confd.C_STR)
-    else:
-        return _confd.ERR_NOT_FOUND
+class DataCallbacks:
+    def cb_get_elem(self, tctx, kp):
+        path = str(kp)
+        if "uptime" in path:
+            delta = datetime.now() - START_TIME
+            val = _confd.Value(str(delta).split('.')[0], _confd.C_STR)
+        elif "last-checked-at" in path:
+            now = datetime.now().strftime("%H:%M:%S")
+            val = _confd.Value(now, _confd.C_STR)
+        else:
+            return _confd.ERR_NOT_FOUND
 
-    dp.data_reply_value(tctx, val)
-    return _confd.OK
+        dp.data_reply_value(tctx, val)
+        return _confd.OK
 
 def run():
     dctx = dp.init_daemon("status_provider_daemon")
 
-    # ソケット作成と接続 (成功したシグネチャを使用)
+    # 接続処理（成功したものを継続）
     ctlsock = socket.socket()
     wrksock = socket.socket()
     dp.connect(dctx, ctlsock, dp.CONTROL_SOCKET, "127.0.0.1", 4565, None)
     dp.connect(dctx, wrksock, dp.WORKER_SOCKET, "127.0.0.1", 4565, None)
     print("Connected successfully!")
 
-    # --- 3. トランザクションコールバックの登録 ---
-    # ここに trans_init と trans_finish を含めるのがポイントです
-    trans_cbs = {
-        'cb_init': trans_init,
-        'cb_finish': trans_finish
-    }
+    # --- 2. クラスのインスタンスを登録 ---
+    # 辞書 {'cb_init': ...} ではなく、オブジェクトそのものを渡します
+    dp.register_trans_cb(dctx, TransCallbacks())
 
-    try:
-        dp.register_trans_cb(dctx, trans_cbs)
-    except Exception:
-        # 万が一 cb_finish でエラーが出た場合は 'finish' に戻してください
-        trans_cbs = {'cb_init': trans_init, 'finish': trans_finish}
-        dp.register_trans_cb(dctx, trans_cbs)
-
-    # --- 4. データコールバックの登録 ---
-    data_cbs = {
-        'get_elem': get_elem_callback
-    }
-    dp.register_data_cb(dctx, "server_status_cp", data_cbs)
+    # データコールバックも同様にクラスで登録
+    # メソッド名が cb_get_elem になっていることに注目
+    dp.register_data_cb(dctx, "server_status_cp", DataCallbacks())
 
     dp.register_done(dctx)
 
-    print("Status Provider is running... (Ready for show commands)")
+    print("Status Provider is running... (Waiting for show server-status)")
 
     try:
         while True:
@@ -76,6 +63,9 @@ def run():
     finally:
         ctlsock.close()
         wrksock.close()
+
+
+
 
 if __name__ == "__main__":
     run()
